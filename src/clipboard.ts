0f4f990dd450c5ab6f881b4ff5c42599835855d5
mrdulin <novaline.dulin@gmail.com>
2024-03-21 11:10:24 +0800

    fix(clipboard): crossOrigin属性的值是小写

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -8,32 +8,32 @@ export async function writeImage(element: HTMLImageElement | null) {
       return;
     }
 
-    // const img = new Image();
+    const img = new Image();
     //浏览器在加载图像时要使用匿名身份验证，以允许跨域资源共享（CORS）。
-    element.crossOrigin = 'Anonymous';
-    // img.src = element.src;
-    // img.onload = () => {
-    //创建一个画布，赋予画布宽高为图片的原始宽高
-    canvas.width = element.naturalWidth;
-    canvas.height = element.naturalHeight;
+    img.crossOrigin = 'anonymous';
+    img.src = element.src;
+    img.onload = () => {
+      //创建一个画布，赋予画布宽高为图片的原始宽高
+      canvas.width = element.naturalWidth;
+      canvas.height = element.naturalHeight;
 
-    //防止有缓存，绘制之前先清除画布
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.drawImage(element, 0, 0);
-    //将canvas转为blob
-    canvas.toBlob((blob) => {
-      if (blob === null) {
-        reject('canvas to blob失败');
-        return;
-      }
-      navigator.clipboard
-        .write([
-          new ClipboardItem({
-            [blob.type]: blob,
-          }),
-        ])
-        .then(resolve, reject);
-    });
-    // };
+      //防止有缓存，绘制之前先清除画布
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.drawImage(img, 0, 0);
+      //将canvas转为blob
+      canvas.toBlob((blob) => {
+        if (blob === null) {
+          reject('canvas to blob失败');
+          return;
+        }
+        navigator.clipboard
+          .write([
+            new ClipboardItem({
+              [blob.type]: blob,
+            }),
+          ])
+          .then(resolve, reject);
+      });
+    };
   });
 }
