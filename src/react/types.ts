cd926b378633a1c2556db81c05ac6f15e7cb8412
mrdulin <novaline.dulin@gmail.com>
2024-09-06 10:37:43 +0800

    feat(types): add GetRef utility type

--- src/react/types.ts
+++ src/react/types.ts
@@ -19,3 +19,13 @@ export type InferRef<T> = T extends ForwardRefExoticComponent<infer Ref>
     ? RefElement
     : never
   : never;
+
+type ReactRefComponent<Props extends { ref?: React.Ref<any> | string }> = (props: Props) => React.ReactNode;
+
+type ExtractRefAttributesRef<T> = T extends React.RefAttributes<infer P> ? P : never;
+
+export type GetRef<T extends ReactRefComponent<any> | React.Component<any>> = T extends React.Component<any>
+  ? T
+  : T extends React.ComponentType<infer P>
+  ? ExtractRefAttributesRef<P>
+  : never;
